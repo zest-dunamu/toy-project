f7762737aa84133b4d25a7f822c6781bb1db7fcc
zest-dunamu <zest@dunamu.com>
2021-08-04 11:41:41 +0900

    chore : Add log util

---
+++ src/main/kotlin/com/zest/toyproject/common/utils/LogUtil.kt
@@ -0,0 +1,10 @@
+package com.zest.toyproject.common.utils
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class LogUtil {
+    companion object {
+        val log: Logger = LogManager.getLogger()
+    }
+}
